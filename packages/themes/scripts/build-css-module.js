3b549ed9c85b19d890f91ea7551a9b2c52736102
rarlala <wtw9611@gmail.com>
2024-10-24 17:53:27 +0900

    feat: add variables colors data, add build-css-module script

---
+++ packages/themes/scripts/build-css-module.js
@@ -0,0 +1,51 @@
+import * as theme from "../dist/index.js";
+import fs from "fs";
+
+const toCssCasting = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, "$1-$2")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase();
+};
+
+const generateThemeCssVariables = () => {
+  const cssString = [];
+
+  Object.entries(theme.vars).forEach(([key, value]) => {
+    if (key === "colors") {
+      console.log(key, "colors");
+      Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
+        console.log(colorKey);
+        if (colorKey === "light") {
+          const selector = ":root";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(
+                      subKey
+                    )}: ${subValue};`
+                )
+                .join("\n")
+            )
+            .join("\n");
+
+          cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
+      });
+    }
+  });
+
+  return cssString;
+};
+
+const generateThemeCss = () => {
+  const variables = generateThemeCssVariables();
+  console.log(variables);
+
+  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+};
+
+generateThemeCss();
