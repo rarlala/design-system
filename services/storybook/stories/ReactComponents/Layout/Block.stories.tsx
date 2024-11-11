d56138934b94f14edd98dfe88210d61f4107e989
rarlala <wtw9611@gmail.com>
2024-11-11 21:25:50 +0900

    feat: add storybook, setting

---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import "@rarla/react-components-layout/style.css";
+import { Block as _Block } from "@rarla/react-components-layout";
+
+export default {
+  title: "React Components/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
