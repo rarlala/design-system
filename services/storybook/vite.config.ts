2b217901e1df5f5ffb8ff86e340cfdeec7868caa
rarlala <wtw9611@gmail.com>
2024-11-12 17:12:55 +0900

    feat: add test setting
    - add vitest

---
+++ services/storybook/vite.config.ts
@@ -0,0 +1,12 @@
+// <reference types="vitest">
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    global: true,
+    include: ["./stories/**/*.test.ts?(x) "],
+  },
+});
